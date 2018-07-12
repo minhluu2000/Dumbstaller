dd1377485c7c0aa46572e6128339bfd1e2242a19
minhluu2000 <40921064+minhluu2000@users.noreply.github.com>
2018-07-12 22:30:44 +0700

    Network Config (Test)
    Finished auto network config (only if the host name matches the data from networkconfigdata.txt). Currently, the code only works if it's run by an admin and if the PC's ethernet card is named "Ethernet".

--- Windows Installer Project/Dumbstaller/Part 1 - Basic installation/ipconfig_test.py
+++ Windows Installer Project/Dumbstaller/Part 1 - Basic installation/ipconfig_test.py
@@ -3,8 +3,6 @@
 # Please DO NOT use it unless you know what you're doing.
 #-------------------------------------------------------------------------------
 
-
-
 import subprocess
 from sys import exit
 
@@ -16,17 +14,11 @@ dns_2 = str(input("Secondary DNS: "))
 
 
 def netmask_to_cidr(netmask):
-    '''
-    :param netmask: netmask ip addr (eg: 255.255.255.0)
-    :return: equivalent cidr number to given netmask ip (eg: 24)
-    '''
     return sum([bin(int(x)).count('1') for x in netmask.split('.')])
 
-num = str(netmask_to_cidr(subnet_mask))
 
-
-subprocess.call(['powershell.exe', r'New-NetIPAddress –InterfaceAlias “Ethernet”  –IPAddress "' + ip_address + r'" -PrefixLength ' + num + r' -DefaultGateway ' + default_gateway])
-subprocess.call(['powershell.exe', r'Set-DnsClientServerAddress -InterfaceAlias “Ethernet” -ServerAddresses' + dns_1 + r', ' + dns_2])
+subprocess.call(['powershell.exe', r'New-NetIPAddress –InterfaceAlias “Ethernet”  –IPAddress "' + ip_address + r'" -PrefixLength ' + str(netmask_to_cidr(subnet_mask)) + r' -DefaultGateway ' + default_gateway])
+subprocess.call(['powershell.exe', r'Set-DnsClientServerAddress -InterfaceAlias “Ethernet” -ServerAddresses ' + dns_1 + r', ' + dns_2])
 
 input("Press enter to exit...")
 exit()
--- Windows Installer Project/Dumbstaller/Part 1 - Basic installation/network_config_test.py
+++ Windows Installer Project/Dumbstaller/Part 1 - Basic installation/network_config_test.py
@@ -13,8 +13,10 @@
 #-------------------------------------------------------------------------------
 
 # import
-import socket, os
+import socket, os, subprocess
 from platform import node
+
+#-------------------import data from networkconfigdata.txt----------------------
 
 
 # check the host name for auto config
@@ -31,57 +33,53 @@ network_config_elements = []  # list of network elements within a list of
 for i in network_configs:
     network_config_elements.append(i.split(","))
 
+##global ip_address
+##global subnet_mask
+##global default_gateway
+##global dns_1
+##global dns_2
+
+#-------------------------------------------------------------------------------
+
+# netmask to cidr function
+# this is used for convert netmask (255.255.255.0) to cidr notation (24)
+def netmask_to_cidr(netmask):
+    return sum([bin(int(x)).count('1') for x in netmask.split('.')])
+
 
 # automatic network config function
-def auto_network_config():
-    # create objects as entries for powershell
-    ip_address = ""
-    subnet_mask = ""
-    default_gateway = ""
-    dns_1 = ""
-    dns_2 = ""
+def network_config(ip_address, subnet_mask, default_gateway, dns_1, dns_2):
 
-    # compare the hostname with any entry in networkconfigdata.txt
-    for i in network_config_elements:
-        if hostname == i[0]: # if there is an entry, move the elements to discrete objects
-            ip_address = i[1]
-            subnet_mask = i[2]
-            default_gateway = i[3]
-            dns_1 = i[4]
-            dns_2 = i[5]
-        else:
-            return False
+    subprocess.call(['powershell.exe', r'New-NetIPAddress –InterfaceAlias “Ethernet”  –IPAddress "' + ip_address + r'" -PrefixLength ' + str(netmask_to_cidr(subnet_mask)) + r' -DefaultGateway ' + default_gateway])
+    subprocess.call(['powershell.exe', r'Set-DnsClientServerAddress -InterfaceAlias “Ethernet” -ServerAddresses ' + dns_1 + r', ' + dns_2])
 
-
-
-
-
-
-
-
-
-# manual network config function
-def manual_network_config():
-    pass
 
 # manual syntax check: make sure the data is written with correct syntax
 def syntax_check():
     pass
 
 
-
 # config export function: export the file
 def config_export():
     pass
 
+#-------------------------------------------------------------------------------
 
 # main function (test for now)
 def main():
-    if auto_network_config():
-        pass
-    else:
-        print("N/A")
+    # only works when the host name matches with data from networkconfigdata.txt
+     for i in network_config_elements:
+        if hostname == i[0]: # if there is an entry, move the elements to discrete objects
+            ip_address = str(i[1])
+            subnet_mask = str(i[2])
+            default_gateway = str(i[3])
+            dns_1 = str(i[4])
+            dns_2 = str(i[5])
+            network_config(ip_address, subnet_mask, default_gateway, dns_1, dns_2)
+        else:
+            print()
 
+main()
 
 
 
