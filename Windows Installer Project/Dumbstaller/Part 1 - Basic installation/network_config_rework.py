34b3a72ca430220bf87d6df974f226819d1f5c71
minhluu2000 <40921064+minhluu2000@users.noreply.github.com>
2018-09-04 08:45:18 -0500

    network_config_rework
    Improved network_config method by combining functionality of netmask_to_cidr with network_config. The netmask_to_cidr method is still being kept for troubleshooting.

--- Windows Installer Project/Dumbstaller/Part 1 - Basic installation/network_config_rework.py
+++ Windows Installer Project/Dumbstaller/Part 1 - Basic installation/network_config_rework.py
@@ -48,12 +48,18 @@ class NetworkConfig:
         self.dns2 = dns2
     
     def netmask_to_cidr(self):
+
         """This method converts TCP/IP to CIDR form for the subnet mask."""
+        
         self.cidr = sum([bin(int(x)).count('1') for x in self.ip4_subnet.split('.')])
-        return sum([bin(int(x)).count('1') for x in self.ip4_subnet.split('.')])
+        return self.cidr
 
     def network_config(self):
-        """This method deploys user network configuration through system-dependent PowerShell (for now)."""
+
+        """This method deploys user network configuration through system-dependent PowerShell (for now). 
+        It also converts TCP/IP to CIDR notation for subnet mask."""
+
+        self.cidr = sum([bin(int(x)).count('1') for x in self.ip4_subnet.split('.')])
         subprocess.call(['powershell.exe', "New-NetIPAddress –InterfaceAlias %s –IPAddress %s -PrefixLength %s -DefaultGateway %s" % (self.netmode, self.ip4, self.cidr, self.gateway)])
         subprocess.call(['powershell.exe', "Set-DnsClientServerAddress -InterfaceAlias %s -ServerAddresses %s, %s" % (self.netmode, self.dns1, self.dns2)])
 
@@ -73,8 +79,7 @@ adapter_choice = int(input("\nYour choice: "))
 
 
 
-host_network_config = NetworkConfig(str(adapters[0].ips[0].nice_name), "192.168.1.10", "255.255.255.0", "192.168.1.1", "1.1.1.1", "8.8.8.8")
-host_network_config.netmask_to_cidr()
+host_network_config = NetworkConfig(str(adapters[0].ips[0].nice_name), "192.168.1.233", "255.255.255.0", "192.168.1.1", "1.1.1.1", "8.8.8.8")
 host_network_config.network_config()
 
 input("Enter...")
