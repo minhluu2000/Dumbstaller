b3afd9ccddedac496d3d107caf121c0dcdee921e
minhluu2000 <40921064+minhluu2000@users.noreply.github.com>
2018-09-23 19:58:19 -0500

    Quick commit
    Re-constructing the file structure. HEHE XD.

--- Windows Installer Project/0README!!!/WUInstall.bat
+++ Windows Installer Project/0README!!!/WUInstall.bat
@@ -1,3 +1,4 @@
 powershell -ExecutionPolicy ByPass -Command Install-Module PSWindowsUpdate
 powershell -ExecutionPolicy ByPass -Command Get-WUInstall -MicrosoftUpdate -AcceptAll -AutoReboot
+powershell -ExecutionPolicy ByPass -Command Get-WindowsUpdate -AcceptAll -Install -IgnoreReboot
 cmd /k
